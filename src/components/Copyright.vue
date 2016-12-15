37573137ec11ddb1976c049636ef66c42eecc75d
jigsawye <jigsaw.ye@gmail.com>
2016-12-15 11:53:03 +0800

    Update source code url

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -5,7 +5,7 @@
         <p>
           <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> By Evan Ye - <a href="https://github.com/jigsawye">@jigsawye</a>
         </p>
-        <p><a class="icon" href="https://github.com"><i class="fa fa-github"></i></a></p>
+        <p><a class="icon" href="https://github.com/jigsawye/ro-mvp-counter"><i class="fa fa-github"></i></a></p>
       </div>
     </div>
   </footer>
